2af0c520814ded3ef6914cf81fefa401a7826713
amitjomy007 <amitjomy007@gmail.com>
2025-07-25 23:04:14 +0530

    vercel deployment navigation error fix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import vercel from 'vite-plugin-vercel';
+ 
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,9 +14,9 @@ export default defineConfig(({ mode }) => {
   return {
     server: {
       host: "::",
-      port: 8080,
+      port: process.env.PORT as unknown as number,
     },
-    plugins: [react(), mode === "development" && componentTagger()].filter(
+    plugins: [vercel(),react(), mode === "development" && componentTagger()].filter(
       Boolean
     ),
     resolve: {
